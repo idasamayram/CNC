53702eb7adb691a5cf5ad840335b7aee0c0e03f3
idasamayram <maryam.asadi.92@hotmail.com>
2026-05-27 10:26:41 +0200

    changed relu layer names for activation analysis distinction

--- Classification/cnn1D_model.py
+++ Classification/cnn1D_model.py
@@ -13,18 +13,26 @@ class CNN1D_Wide(nn.Module):
     def __init__(self):
         super(CNN1D_Wide, self).__init__()
         self.conv1 = nn.Conv1d(3, 16, kernel_size=25, stride=1, padding=12)
+        self.relu1 = nn.LeakyReLU(0.1)
+
         self.pool1 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.dropout1 = nn.Dropout(0.2)
 
         self.conv2 = nn.Conv1d(16, 32, kernel_size=15, stride=1, padding=7)
+        self.relu2 = nn.LeakyReLU(0.1)
+
         self.pool2 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.dropout2 = nn.Dropout(0.2)
 
         self.conv3 = nn.Conv1d(32, 64, kernel_size=9, stride=1, padding=4)
+        self.relu3 = nn.LeakyReLU(0.1)
+
         self.pool3 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.dropout3 = nn.Dropout(0.2)
 
         self.conv4 = nn.Conv1d(64, 128, kernel_size=5, stride=1, padding=2)
+        self.relu4 = nn.LeakyReLU(0.1)
+
         self.pool4 = nn.MaxPool1d(kernel_size=2, stride=2)
         self.dropout4 = nn.Dropout(0.2)
 
@@ -33,7 +41,7 @@ class CNN1D_Wide(nn.Module):
         self.fc2 = nn.Linear(64, 2)
 
         self.dropout = nn.Dropout(0.4)
-        self.relu = nn.LeakyReLU(0.1)
+        self.relu_fc = nn.LeakyReLU(0.1)
 
         self._initialize_weights()
 
@@ -48,13 +56,13 @@ class CNN1D_Wide(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        x = self.dropout1(self.pool1(self.relu(self.conv1(x))))
-        x = self.dropout2(self.pool2(self.relu(self.conv2(x))))
-        x = self.dropout3(self.pool3(self.relu(self.conv3(x))))
-        x = self.dropout4(self.pool4(self.relu(self.conv4(x))))
+        x = self.dropout1(self.pool1(self.relu1(self.conv1(x))))
+        x = self.dropout2(self.pool2(self.relu2(self.conv2(x))))
+        x = self.dropout3(self.pool3(self.relu3(self.conv3(x))))
+        x = self.dropout4(self.pool4(self.relu4(self.conv4(x))))
 
         x = self.global_avg_pool(x).squeeze(-1)
-        x = self.relu(self.fc1(x))
+        x = self.relu_fc(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
         return x
